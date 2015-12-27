4e134e5b49bd610bc499f65451ae7b149a192725
Legokichi Duckscallion <legokichi@gmail.com>
2015-12-27 17:14:53 +0900

    boost version

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <boost/version.hpp>
 
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    int major = BOOST_VERSION / 100000;
+    int minor = BOOST_VERSION / 100 % 1000;
+    int patch = BOOST_VERSION % 100;
+    std::cout << BOOST_VERSION << std::endl;
+    std::cout << "boost version " << major << "." << minor << "." << patch
+    << " or " << BOOST_LIB_VERSION << std::endl;
     return 0;
 }
